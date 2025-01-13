25bdf494f832e8b75585405bcbff0fb720515a65
Tanmay Aswal <tanmayaswal80@gmail.com>
2025-01-13 18:27:21 +0530

    Updated Schema and added webhook

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express"
+import db from "@repo/db/client"
+const app=express();
+
+app.post("/hdfcWebhook",async (req,res)=>{
+    const paymentInformation={
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    //Transaction
+    try{
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                data:{
+                    amount:{
+                        increment:paymentInformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where:{
+                    token: paymentInformation.token
+                },
+                data:{
+                    status:"Success"
+                }
+            })
+        ])
+
+        res.json({
+            message:"Captured"
+        })
+    }catch(e){
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
+    
+})
+
+app.listen(3003);
